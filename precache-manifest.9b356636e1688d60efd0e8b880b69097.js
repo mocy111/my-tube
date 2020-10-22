16d7d5367f6aed5309a344468783513172a1aff8
MOCTAR YONLI <yonli.moctar@codeloccol.org>
2020-10-22 18:33:49 +0100

    Updates

---
+++ precache-manifest.9b356636e1688d60efd0e8b880b69097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7322d383b84dd4f956840125e442e272",
+    "url": "/my-tube/index.html"
+  },
+  {
+    "revision": "07149ff8f86fc589976c",
+    "url": "/my-tube/static/js/2.1dd536c1.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/my-tube/static/js/2.1dd536c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3471c7ced19576ae0c",
+    "url": "/my-tube/static/js/main.750396a6.chunk.js"
+  },
+  {
+    "revision": "c4c9b4fcb8574ac9106b",
+    "url": "/my-tube/static/js/runtime-main.4d54c6d3.js"
+  }
+]);
